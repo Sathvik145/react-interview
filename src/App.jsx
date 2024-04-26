f1eeb76d3e7a77e169a3001bdd78c2af97b687ee
Sathvik145 <sathviknampelli@gmail.com>
2024-04-26 14:01:28 +0530

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import FeedbackModal from './components/FeedbackModal'
 
 function App() {
 
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)  //use state initially closed default
 
-  const openModal = () => {
+  const openModal = () => {       //fn to set isModelOpen true
     setIsModalOpen(true)
   }
 
-  const closeModal = () => {
+  const closeModal = () => {      //fn to set isMOdelOpen false
     setIsModalOpen(false)
   }
 
@@ -18,10 +18,10 @@ function App() {
     <div>
       <button className='feedback-btn' onClick={openModal}>Feedback</button>
       {
-        isModalOpen && 
-          <FeedbackModal onClose={closeModal} />
-      }
-    </div>
+        isModalOpen &&          //when isModelOpen is true it will fire the Feedback component
+          <FeedbackModal onClose={closeModal} />  //and when we onclick onclose in the feedback model near the cross button it will fire the onclose fn which wil call the close model 
+      }                                          
+    </div>                                         //which will make the isMOdel open false so the box closes
   )
 }
 
